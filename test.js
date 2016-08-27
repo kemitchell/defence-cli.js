fc93ca67580b5b13c63ef354b548d048e03624db
Kyle E. Mitchell <kyle@kemitchell.com>
2016-08-26 17:45:34 -0700

    Use JavaScript Standard Style

--- test.js
+++ test.js
@@ -3,22 +3,23 @@ var fs = require('fs')
 var path = require('path')
 var tape = require('tape')
 
-function runCommand(input, args) {
-  return exec('./defence', args).toString() }
-
-tape('JavaScript only', function(test) {
+tape('JavaScript only', function (test) {
   var markdown = fs.realpathSync('examples/input.md')
-  require('glob').sync('examples/*.output')
-    .forEach(function(outputFile) {
-      var flags = require('./' + outputFile.replace('.output', '.json'))
-      var output = fs.readFileSync(outputFile).toString()
-      test.equal(
-        exec(
-          [ './defence' ]
-            .concat(flags)
-            .concat(markdown)
-            .join(' '))
-          .toString(),
-        output,
-        path.basename(outputFile, '.output')) })
-  test.end() })
+  require('glob')
+  .sync('examples/*.output')
+  .forEach(function (outputFile) {
+    var flags = require('./' + outputFile.replace('.output', '.json'))
+    var output = fs.readFileSync(outputFile).toString()
+    test.equal(
+      exec(
+        ['./defence']
+        .concat(flags)
+        .concat(markdown)
+        .join(' ')
+      )
+      .toString(),
+      output,
+      path.basename(outputFile, '.output'))
+  })
+  test.end()
+})
