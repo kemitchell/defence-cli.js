5b6d7978e5e9cc846ae60638791751a5286b8e42
Kyle E. Mitchell <kyle@kemitchell.com>
2019-04-13 17:42:18 -0700

    standard@12.0.1

--- test.js
+++ test.js
@@ -6,20 +6,19 @@ var tape = require('tape')
 tape('JavaScript only', function (test) {
   var markdown = fs.realpathSync('examples/input.md')
   require('glob')
-  .sync('examples/*.output')
-  .forEach(function (outputFile) {
-    var flags = require('./' + outputFile.replace('.output', '.json'))
-    var output = fs.readFileSync(outputFile).toString()
-    test.equal(
-      exec(
-        ['./defence']
-        .concat(flags)
-        .concat(markdown)
-        .join(' ')
-      )
-      .toString(),
-      output,
-      path.basename(outputFile, '.output'))
-  })
+    .sync('examples/*.output')
+    .forEach(function (outputFile) {
+      var flags = require('./' + outputFile.replace('.output', '.json'))
+      var output = fs.readFileSync(outputFile).toString()
+      test.equal(
+        exec(
+          ['./defence']
+            .concat(flags)
+            .concat(markdown)
+            .join(' ')
+        ).toString(),
+        output,
+        path.basename(outputFile, '.output'))
+    })
   test.end()
 })
